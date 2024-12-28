a0a0da45dc68864ee0f7470d75544aaad4787bec
Born2Student <shawnyangxw@hotmail.com>
2024-12-28 10:10:06 +0800

    Add files via upload

---
+++ Treasure_Trekker_SG/Treasure_Trekker_SGTests/Treasure_Trekker_SGTests.swift
@@ -0,0 +1,17 @@
+//
+//  Treasure_Trekker_SGTests.swift
+//  Treasure_Trekker_SGTests
+//
+//  Created by Shawn Yang on 26/9/24.
+//
+
+import Testing
+@testable import Treasure_Trekker_SG
+
+struct Treasure_Trekker_SGTests {
+
+    @Test func example() async throws {
+        // Write your test here and use APIs like `#expect(...)` to check expected conditions.
+    }
+
+}
---
+++ Treasure_Trekker_SG/Treasure_Trekker_SGUITests/Treasure_Trekker_SGUITestsLaunchTests.swift
@@ -0,0 +1,33 @@
+//
+//  Treasure_Trekker_SGUITestsLaunchTests.swift
+//  Treasure_Trekker_SGUITests
+//
+//  Created by Shawn Yang on 26/9/24.
+//
+
+import XCTest
+
+final class Treasure_Trekker_SGUITestsLaunchTests: XCTestCase {
+
+    override class var runsForEachTargetApplicationUIConfiguration: Bool {
+        true
+    }
+
+    override func setUpWithError() throws {
+        continueAfterFailure = false
+    }
+
+    @MainActor
+    func testLaunch() throws {
+        let app = XCUIApplication()
+        app.launch()
+
+        // Insert steps here to perform after app launch but before taking a screenshot,
+        // such as logging into a test account or navigating somewhere in the app
+
+        let attachment = XCTAttachment(screenshot: app.screenshot())
+        attachment.name = "Launch Screen"
+        attachment.lifetime = .keepAlways
+        add(attachment)
+    }
+}
